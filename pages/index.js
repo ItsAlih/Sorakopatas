34a40e3f097b0ec18f6fa33858fa19a59e897c89
Alice <75282381+ItsAlih@users.noreply.github.com>
2021-02-25 02:34:59 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,25 @@
-const onClickButton = () => {
+import Head from "next/head";
+import icon from "../img/favicon.jpg";
+
+const creatorLinkButton = () => {
   window.location.replace("https://twitter.com/SorakaAgressiva");
+};
+
+const discordInviteButton = () => {
+  window.location.replace("https://discord.gg/RVFpfx6");
 };
 
 function Home() {
   return (
     <div id="toor">
+      <Head>
+        <link rel="shortcut icon" href={icon} />
+      </Head>
       <div className="team">
         <nav className="navbar navbar-dark bg-dark">
-          <a className="navbar-brand">Sorakopatas</a>
+          <a className="navbar-brand">
+            We are <strong style={{ color: "yellow" }}>Sorakopatas.</strong>
+          </a>
           <div className="form-inline">
             <input
               className="form-control mr-sm-2"
@@ -25,7 +37,7 @@ function Home() {
           style={{ marginTop: "36px" }}
         >
           <h1 className="text-center" style={{ fontSize: "3rem" }}>
-            Sorakopatas
+            We are <strong style={{ color: "yellow" }}>Sorakopatas!</strong>
           </h1>
           <br />
           <p className="text-center" style={{ fontSize: "20px" }}>
@@ -37,23 +49,39 @@ function Home() {
             . <br />
           </p>
           <p className="text-center txt-opc" style={{ fontSize: "20px" }}>
-            Caso esteja interessado, entre no nosso servidor, temos uma <br />
-            área focada para desenvolvedores, jogos, Genshin Impact e diversos
-            assuntos. <br />
+            Caso esteja interessado, entre no nosso servidor, temos uma área{" "}
+            focada
+            <br /> para desenvolvedores, jogos, Genshin Impact, Minecraft e
+            diversos assuntos. <br />
             Não somos apenas amigos, somos uma família! <br />
           </p>
           <div style={{ display: "flex", justifyContent: "center" }}>
-            <button className="btn btn-success" onClick={onClickButton}>
+            <button className="btn btn-success" onClick={creatorLinkButton}>
               Conheça nossa criadora!
+            </button>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              marginTop: "16px",
+            }}
+          >
+            <button className="btn btn-success" onClick={discordInviteButton}>
+              Entre em nosso servidor!
             </button>
           </div>
         </div>
         <br />
-        <footer style={{ marginTop: "10rem" }}>
+        <footer style={{ marginTop: "6rem" }}>
           Created by:{" "}
           <a href="https://twitter.com/NenemDaMya" style={{ color: "yellow" }}>
             Alice
-          </a>
+          </a>{" "}
+          <br />
+          <p style={{ marginTop: "8px", fontSize: "16px" }}>
+            Proibido <strong style={{ color: "red" }}>Jannistas.</strong>
+          </p>
         </footer>
       </div>
     </div>
